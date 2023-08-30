38f248a30fe0b521f375175e6312c081923be829
mauriciogaray10 <garaycarlosmauricio17@gmail.com>
2023-08-30 14:54:42 -0300

    firebase initialized

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+//  web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtk8IEp3BxpsTFgpliHxomJRXKgPxR4l8",
+  authDomain: "cmecommerce-ae1b8.firebaseapp.com",
+  projectId: "cmecommerce-ae1b8",
+  storageBucket: "cmecommerce-ae1b8.appspot.com",
+  messagingSenderId: "643832522039",
+  appId: "1:643832522039:web:621fb893346dd32d37a6b2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
