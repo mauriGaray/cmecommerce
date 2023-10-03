731f8b4a791bc55ef23d57882f0a0d6867923bb2
mauriciogaray10 <garaycarlosmauricio17@gmail.com>
2023-10-03 17:24:34 -0300

    addproduct v

--- src/components/pages/addProduct/AddProduct.jsx
+++ src/components/pages/addProduct/AddProduct.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
 import styles from "./addProduct.module.css";
 
 function AddProduct() {
+  const [category, setCategory] = useState("");
+  const [productName, setProductName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [stock, setStock] = useState(0);
+  const [descount, setDescount] = useState(0);
+  const [monthly, setMonthly] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let data = {
+      category,
+      productName,
+      description,
+      price,
+      stock,
+      descount,
+      monthly,
+      image,
+    };
+    console.log(data);
+  };
+
   return (
-    <form action="submit" className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <ul className={styles.ul}>
         <li>
           <h1>CREAR NUEVO ITEM</h1>
@@ -10,8 +35,8 @@ function AddProduct() {
         <li className={`${styles.typography}, ${styles.div}`}>
           <div>
             Categoría:
-            <select name="select" className={styles.select}>
-              <option value="General" selected>
+            <select name="category" className={styles.select}>
+              <option value="General" defaultValue>
                 General
               </option>
               <option value="Electrodomesticos">Electrodomésticos</option>
@@ -28,14 +53,24 @@ function AddProduct() {
         </li>
         <li className={styles.typography}>
           <label htmlFor="productData">Nombre del producto: </label>
-          <input type="text" name="productData" placeholder=" Heladera..." />
+          <input
+            type="text"
+            name="productName"
+            placeholder=" Heladera..."
+            onChange={(event) => {
+              setProductName(event.target.value);
+            }}
+          />
         </li>
         <li className={styles.typography}>
           <textarea
             name="productData"
             rows={10}
             cols={91}
-            placeholder=" Descripción del producto"></textarea>
+            placeholder=" Descripción del producto"
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}></textarea>
         </li>
         <li className={`${styles.typography}, ${styles.div2}`}>
           <div>
@@ -43,13 +78,24 @@ function AddProduct() {
             <input
               size={9}
               type="text"
-              name="precio"
+              name="price"
               placeholder=" $0.000,00"
+              onChange={(event) => {
+                setPrice(event.target.value);
+              }}
             />
           </div>
           <div>
             <label htmlFor="stock">Stock:</label>
-            <input size={11} type="text" name="stock" placeholder="0" />
+            <input
+              size={11}
+              type="text"
+              name="stock"
+              placeholder="0"
+              onChange={(event) => {
+                setStock(event.target.value);
+              }}
+            />
           </div>
         </li>
         <li className={`${styles.typography}, ${styles.div}`}>
@@ -58,16 +104,18 @@ function AddProduct() {
             <input
               size={1}
               type="number"
-              id="descount"
               name="descount"
               min="0"
               max="100"
               placeholder="0%"
+              onChange={(event) => {
+                setDescount(event.target.value);
+              }}
             />
           </div>
           <div>
             Cuotas:
-            <select name="cuotas" className={styles.select}>
+            <select name="monthly" className={styles.select}>
               <option value="3">3 cuotas sin interés</option>
               <option value="6" selected>
                 6 cuotas sin interés
@@ -77,19 +125,20 @@ function AddProduct() {
           </div>
         </li>
         <li className={styles.typography}>
-          <label htmlFor="image">Imágenes:</label> <input type="file" />
+          <label htmlFor="image">Imágenes:</label>{" "}
+          <input
+            type="file"
+            name="image"
+            onChange={(event) => {
+              setImage(event.target.value);
+            }}
+          />
         </li>
       </ul>
-      <button
-        className={`${styles.gap}, ${styles.btn}`}
-        type="submit"
-        value="addProduct">
+      <button className={`${styles.gap}, ${styles.btn}`} type="submit">
         Agregar Producto
       </button>
-      <button
-        className={`${styles.gap}, ${styles.btn}`}
-        type="reset"
-        value="clean">
+      <button className={`${styles.gap}, ${styles.btn}`} type="reset">
         Limpiar
       </button>
     </form>
